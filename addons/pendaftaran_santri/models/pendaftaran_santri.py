d8651049f4d153f16597786a1b938f4643cdf2c6
afif <saipulapip.cep@gmail.com>
2024-02-01 15:41:59 +0700

    odoo-16-pendaftaran_santri

--- addons/pendaftaran_santri/models/pendaftaran_santri.py
+++ addons/pendaftaran_santri/models/pendaftaran_santri.py
@@ -4,7 +4,7 @@ class PendaftaranSantri(models.Model):
 	_name           = 'pendaftaran.santri'
 	_description    = 'pendaftaran.santri'
 
-	name        = fields.Char(string='Nama Santri', required=True)
+	name        = fields.Char(string='Nama Santri', compute='_compute_name', store=True)
 	tempat_tgl_lahir = fields.Char(string='Tempat Tgl Lahir', required=True)
 	tempat_tgl_lahir2 = fields.Char(string='Tempat Tgl Lahir', required=True)
 	description = fields.Text(string='Orang Tua')
@@ -17,6 +17,11 @@ class PendaftaranSantri(models.Model):
 	infaq = fields.Char(string='Infaq Bulanan', required=True)
 	state = fields.Selection(string='Status', selection=[('draft', 'Draft'), ('confirm', 'Sedang Berlangsung'),('done', 'Selesai'),],default='draft', tracking=True)
 
+	@api.depends('create_uid')
+	def _compute_name(self):
+		for record in self:
+			record.name = record.create_uid.name
+			
 	def action_confirm(self):
 				self.state='confirm'
 				
@@ -25,3 +30,9 @@ class PendaftaranSantri(models.Model):
 
 	def action_draft(self):
 				self.state='draft'	
+
+	@api.constrains('mobile')
+	def _check_mobile(self):
+		for record in self:
+			if record.mobile and not record.mobile.isdigit():
+				raise models.ValidationError('No. HP hanya boleh diisi dengan angka')
