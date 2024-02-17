a56f8c94163cb8644ee6f619d435e39e80654543
afif <saipulapip.cep@gmail.com>
2024-02-17 09:57:18 +0700

    pembaruan

--- addons/pendaftaran_santri/models/pendaftaran_santri.py
+++ addons/pendaftaran_santri/models/pendaftaran_santri.py
@@ -4,7 +4,7 @@ class PendaftaranSantri(models.Model):
 	_name           = 'pendaftaran.santri'
 	_description    = 'pendaftaran.santri'
 
-	name        = fields.Char(string='Nama Santri', required=True)
+	name        = fields.Char(string='Nama Santri',  compute='_compute_name', required=True)
 	jenis_kel = fields.Selection(string='Jenis Kelamin', selection=[('L', 'Laki-Laki'), ('P', 'Perempuan'),])
 	tempat_lahir = fields.Char(string='Tempat Lahir',)
 	tgl_lahir = fields.Date(string='Tanggal Lahir')
@@ -14,13 +14,20 @@ class PendaftaranSantri(models.Model):
 	dari = fields.Char(string='Dari berapa saudara')
 	Alamat = fields.Char(string='Alamat Tinggal')
 	ibu = fields.Char(string='Nama Ibu Kandung')
+	pekerjan1 = fields.Char(string='Pekerjaan')
 	tlpn_ibu = fields.Char(string='No Telpon Ibu')
 	ayah = fields.Char(string='Nama Ayah Kandung')
+	pekerjan2 = fields.Char(string='Pekerjaan')
 	tlpn_ayah = fields.Char(string='No Telpon Ayah')
 	sekolah = fields.Char(string='Sekolah Asal')
 	dituju = fields.Char(string='Pendidikan Dituju')
 	image_1920 = fields.Binary(string='Foto Santri')
 	state = fields.Selection(string='Status', selection=[('draft', 'Draft'), ('confirm', 'Sedang Berlangsung'),('done', 'Selesai'),],default='draft', tracking=True)
+
+	@api.depends('create_uid')
+	def _compute_name(self):
+			for record in self:
+				record.name = record.create_uid.name
 
 			
 	def action_confirm(self):
